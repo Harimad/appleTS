4a725778e992488f95e40d6b6e50ab9dc7bb026b
Harimad <leesh68481@gmail.com>
2022-11-14 01:00:41 +0900

    [JS] slide Nav Pagination

--- JsProject/slideNavBtn/main.js
+++ JsProject/slideNavBtn/main.js
@@ -2,6 +2,7 @@ const $slide = document.querySelector('.slide')
 const $leftBtn = document.querySelector('.slide-btn-left')
 const $rightBtn = document.querySelector('.slide-btn-right')
 const $slideItems = document.querySelectorAll('.slide-box')
+const $pagination = document.querySelector('.slide_pagination')
 
 let slideWidth = $slide.clientWidth // 슬라이드 너비
 let slideNums = $slideItems.length // 슬라이드 개수
@@ -45,3 +46,32 @@ $leftBtn.addEventListener('click', leftBtnEvent)
 window.addEventListener('resize', () => {
   slideWidth = $slide.clientWidth // 슬라이드 너비
 })
+
+// 페이지네이션 생성
+for (let i = 0; i < slideNums; i++) {
+  if (i === 0) {
+    $pagination.innerHTML += `<li class="active">•</li>`
+  } else {
+    $pagination.innerHTML += `<li>•</li>`
+  }
+}
+
+const $paginationItems = document.querySelectorAll('.slide_pagination > li')
+console.log($paginationItems)
+
+// 페이지네이션 클릭 할 때 해당 슬라이드로 이동
+for (let i = 0; i < slideNums; i++) {
+  // 각 페이지네이션 마다 클릭 이벤트 추가
+  $paginationItems[i].addEventListener('click', () => {
+    currentSlide = i + 1 // curSlide의 시작 위치가 1이기 때문
+    const offset = slideWidth * (currentSlide - 1) // 슬라이드 이동을 위한 offset 계산
+    console.log(slideWidth, currentSlide, slideWidth * (currentSlide - 1))
+    // 각 슬라이드 아이템의 left에 offset 적용
+    $slideItems.forEach(item => {
+      item.setAttribute('style', `left: ${-offset}px`)
+    })
+    // 슬라이드 이동 할 때 현재 활성화된 pagination 변경
+    $paginationItems.forEach(item => item.classList.remove('active'))
+    $paginationItems[currentSlide - 1].classList.add('active')
+  })
+}
